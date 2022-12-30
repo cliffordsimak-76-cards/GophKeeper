5a41d514375c03f2c22f3928473ad93692992fb9
CliffordSimak <holamellamosasha@gmail.com>
2022-12-30 12:24:20 +0300

    print build and version

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -7,7 +7,14 @@ import (
 	"github.com/cliffordsimak-76-cards/gophkeeper/internal/client"
 )
 
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+)
+
 func main() {
+	printBuildData()
+
 	cfg, err := client.NewConfig()
 	if err != nil {
 		log.Fatal("error running client: ", err)
@@ -16,4 +23,9 @@ func main() {
 	if err = client.Run(context.Background(), cfg); err != nil {
 		log.Fatal("error running client: ", err)
 	}
+}
+
+func printBuildData() {
+	log.Printf("Build version: %v\n", buildVersion)
+	log.Printf("Build date: %v\n", buildDate)
 }
